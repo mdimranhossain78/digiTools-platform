bedae364376731cef376bd6b966b845c35e51557
mdimranhossain78 <mdimrahossan146@gmail.com>
2026-04-02 18:45:03 +0600

    change empty section text

--- src/component/emtyCart/EmtyCart.jsx
+++ src/component/emtyCart/EmtyCart.jsx
@@ -6,7 +6,7 @@ const EmtyCart = () => {
             <div className='text-center pt-25' >
                 
 
-                <h2 className='font-bold text-2xl'>You are not added to cart </h2>
+                <h2 className='font-bold text-2xl'>Your cart is empty </h2>
                 <p>Pless add to cart</p>
             </div>
         </div>
